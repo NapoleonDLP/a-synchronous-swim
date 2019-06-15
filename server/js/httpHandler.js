3805e620422ce140d6e5aae5212d5f29a32a2cc3
NapoleonDLP <napoleon.dlp@gmail.com>
2019-06-14 20:11:11 -0700

    Renders using GET req

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -14,13 +14,15 @@ module.exports.initialize = (queue) => {
 
 module.exports.router = (req, res, next = ()=>{}) => {
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  console.log('Req.method ', req.method)
   if(req.method === 'GET') {
     var moves = ['left', 'right', 'up', 'down'];
     var move = moves[Math.floor(Math.random()* 4)]
     res.writeHead(200, headers);
     res.write(move);
     res.end();
-  }
+  } else {
+  res.writeHead(200, headers);
+  res.end();
   next(); // invoke next() at the end of a request to help with testing!
+  }
 };
